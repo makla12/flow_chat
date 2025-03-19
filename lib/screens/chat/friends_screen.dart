6a822a1349488688fb5888fa203efa7dd4824044
makla12 <1234makla@gmail.com>
2025-03-19 12:45:49 +0100

    add unreaded effect to private messages

--- lib/screens/chat/friends_screen.dart
+++ lib/screens/chat/friends_screen.dart
@@ -111,17 +111,20 @@ class FriendsScreen extends StatelessWidget {
                         final friendName = friendData['username'] as String;
                         final friendAvatarUrl =
                             friendData['avatarUrl'] as String;
+                        final bool isReed = friends[index]['reed'].contains(FirebaseAuth.instance.currentUser!.uid);
                         return ListTile(
                           leading: CircleAvatar(
                             backgroundImage: NetworkImage(friendAvatarUrl),
                           ),
-                          title: Text(friendName),
+                          title: Text(friendName, style: !isReed ? const TextStyle(fontWeight: FontWeight.bold) : null),
                           subtitle: Row(
                             children: [
                               Text(friends[index]['lastMessage']['name'] == FirebaseAuth.instance.currentUser!.uid
                                   ? 'Ty: '
                                   : ''),
-                              Text(friends[index]['lastMessage']['message']),
+                              Text(friends[index]['lastMessage']['message'], style: !isReed
+                                  ? const TextStyle(fontWeight: FontWeight.bold, color: Colors.white)
+                                  : null),
                             ],
                           ),
                           onTap: () {
